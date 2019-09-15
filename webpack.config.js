e1d33a61b73bd914f0a6162c315e526c66834fa1
korthj <khj12005@gmail.com>
2019-09-15 16:32:43 +0900

    삭제

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = function(webpackConfig) {
-  webpackConfig.babel.plugins.push('transform-runtime');
-  webpackConfig.babel.plugins.push(['import', {
-    libraryName: 'antd',
-    style: 'css', 
-  }]);
-  return webpackConfig;
-};
